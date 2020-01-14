bc4093bfd97d64ee2e2f4253c37c08287951c6b8
DarkMatterMatt <mattmoran2000@gmail.com>
2020-01-14 20:15:20 +1300

    Add --skip parameter

--- build-assets.py
+++ build-assets.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 from optparse import OptionParser
 import sys
 import os
@@ -17,13 +17,20 @@ parser.add_option("-i", "--ios",
 parser.add_option("-a", "--android",
                   help="location of Android app root", metavar="ANDROID_PATH")
 parser.add_option("-c", action="store_true", dest="cleanup", help="clean up files after execution")
+parser.add_option("--skip", help="Skip step numbers")
 
 (options, args) = parser.parse_args()
 
+skip = options.skip.split(",")
+
 filename = 'dnzsl-xmldump.xml'
 
-print("Step 1: Fetching the latest signs from Freelex")
-freelex.fetch_database(filename)
+if "1" not in skip:
+    print("Step 1: Fetching the latest signs from Freelex")
+    updated = freelex.update_database(filename)
+    if updated == False:
+        print("You're all up to date, no changes were made")
+        sys.exit(0)
 
 with open(filename) as f:
     data = f.read()
@@ -36,110 +43,115 @@ data = re.sub(r"&(?=[^#])", "&#038;", data)
 parser = ET.XMLParser(encoding="UTF-8")
 root = ET.XML(data, parser=parser)
 
-print("Step 2: Fetching images from freelex")
-freelex.fetch_assets(root)
+if "2" not in skip:
+    print("Step 2: Fetching images from freelex")
+    freelex.fetch_assets(root)
 
-print("Step 3: Rename all images to meet Android requirements")
-freelex.rename_assets(root)
+if "4" not in skip:
+    print("Step 4: Write out nzsl.dat for Android")
+    freelex.write_datfile(root)
 
-print("Step 4: Write out nzsl.dat for Android")
-freelex.write_datfile(root)
+if "5" not in skip:
+    print("Step 5: Write out sqlite nzsl.db for iOS")
+    freelex.write_sqlitefile(root)
 
-print("Step 5: Write out sqlite nzsl.db for iOS")
-freelex.write_sqlitefile()
+if "6" not in skip:
+    print("Step 6: Merge images together into one folder")
+    freelex.copy_images_to_one_folder()
 
-print("Step 6: Merge images together into one folder")
-freelex.copy_images_to_one_folder()
+    if "6a" not in skip:
+        print("Step 6a: Generate search thumbnails")
+        # Create thumbnails used on search screens
+        for path, dirs, files in os.walk("assets/"):
+            for filename in files:
+                # Some images have a 1px border that looks bad in search results
+                # Not all do - but we can safely trim 1px off all images
+                shave_cmd = "mogrify -shave 1x1 assets/" + filename
+                print_run_msg(shave_cmd)
+                os.system(shave_cmd)
 
-print("Step 6a: Generate search thumbnails")
-# Create thumbnails used on search screens
-for path, dirs, files in os.walk("assets/"):
-    for filename in files:
-        # Some images have a 1px border that looks bad in search results
-        # Not all do - but we can safely trim 1px off all images
-        shave_cmd = "mogrify -shave 1x1 assets/" + filename
-        print_run_msg(shave_cmd)
-        os.system(shave_cmd)
+                # Then we make thumbnails of the border-free images
+                create_thumbnail_cmd = "convert -resize x92 assets/" + filename + " assets/50." + filename
+                print_run_msg(create_thumbnail_cmd)
+                os.system(create_thumbnail_cmd)
 
-        # Then we make thumbnails of the border-free images
-        create_thumbnail_cmd = "convert -resize x92 assets/" + filename + " assets/50." + filename
-        print_run_msg(create_thumbnail_cmd)
-        os.system(create_thumbnail_cmd)
+    if "6b" not in skip:
+        print("Step 6b: Shrink images for distribution")
+        # In order to keep the app size small, we need to run a series
+        # of compressions over the images
 
-print("Step 6b: Shrink images for distribution")
-# In order to keep the app size small, we need to run a series
-# of compressions over the images
+        # Resize images larger than 600x600 down using mogrify from imagemagick
+        for path, dirs, files in os.walk("assets/"):
+            for filename in files:
+                cmd = "mogrify -resize '600x600>' assets/" + filename
+                print_run_msg(cmd)
+                os.system(cmd)
 
-# Resize images larger than 600x600 down using mogrify from imagemagick
-for path, dirs, files in os.walk("assets/"):
-    for filename in files:
-        cmd = "mogrify -resize '600x600>' assets/" + filename
-        print_run_msg(cmd)
-        os.system(cmd)
+        # Convert all images to 4 colour depth
+        for path, dirs, files in os.walk("assets/"):
+            for filename in files:
+                recolor_cmd = "convert -colors 4 assets/" + filename + " assets/" + filename
+                print_run_msg(recolor_cmd)
+                os.system(recolor_cmd)
 
-# Convert all images to 4 colour depth
-for path, dirs, files in os.walk("assets/"):
-    for filename in files:
-        recolor_cmd = "convert -colors 4 assets/" + filename + " assets/" + filename
-        print_run_msg(recolor_cmd)
-        os.system(recolor_cmd)
+        # Finally, run optipng
+        for path, dirs, files in os.walk("assets/"):
+            for filename in files:
+                optipng_cmd = "optipng -quiet assets/" + filename
+                print_run_msg(optipng_cmd)
+                os.system(optipng_cmd)
 
-# Finally, run optipng
-for path, dirs, files in os.walk("assets/"):
-    for filename in files:
-        optipng_cmd = "optipng -quiet assets/" + filename
-        print_run_msg(optipng_cmd)
-        os.system(optipng_cmd)
+if "7" not in skip and options.ios is not None:
+    if "7a" not in skip:
+        print("Step 7a: Update iOS app images")
 
+        ios_asset_path = options.ios + "/Data/picture/"
 
-if options.ios is not None:
-    print("Step 7a: Update iOS app images")
+        if os.path.isdir(ios_asset_path):
+            shutil.rmtree(ios_asset_path)
 
-    ios_asset_path = options.ios + "/Data/picture/"
+        os.makedirs(ios_asset_path)
 
-    if os.path.isdir(ios_asset_path):
-        shutil.rmtree(ios_asset_path)
+        # re-create the .gitkeep file in the assets dir (it is not necessary for the
+        # app but keeps that dir in the git repo)
+        os.system("touch " + ios_asset_path + ".gitkeep")
 
-    os.makedirs(ios_asset_path)
+        for path, dirs, files in os.walk("assets/"):
+            for filename in files:
+                cp_cmd = "cp assets/" + filename + " " + ios_asset_path
+                print_run_msg(cp_cmd)
+                os.system(cp_cmd)
 
-    # re-create the .gitkeep file in the assets dir (it is not necessary for the
-    # app but keeps that dir in the git repo)
-    os.system("touch " + ios_asset_path + ".gitkeep")
+    if "7b" not in skip:
+        print("Step 7b: Update iOS app nzsl.db")
+        os.system("cp nzsl.db " + options.ios + "/Data/")
 
-    for path, dirs, files in os.walk("assets/"):
-        for filename in files:
-            cp_cmd = "cp assets/" + filename + " " + ios_asset_path
-            print_run_msg(cp_cmd)
-            os.system(cp_cmd)
+if "8" not in skip and options.android is not None:
+    if "8a" not in skip:
+        print("Step 8a: Update Android app images")
+        android_asset_path = options.android + "/app/src/main/assets/images/signs/"
 
-    print("Step 7b: Update iOS app nzsl.db")
-    os.system("cp nzsl.db " + options.ios + "/Data/")
+        if os.path.isdir(android_asset_path):
+            shutil.rmtree(android_asset_path)
 
-if options.android is not None:
-    print("Step 8a: Update Android app images")
-    android_asset_path = options.android + "/app/src/main/assets/images/signs/"
+        os.makedirs(android_asset_path)
 
-    if os.path.isdir(android_asset_path):
-        shutil.rmtree(android_asset_path)
+        # re-create the .gitkeep file in the assets dir (it is not necessary for the
+        # app but keeps that dir in the git repo)
+        os.system("touch " + android_asset_path + ".gitkeep")
 
-    os.makedirs(android_asset_path)
+        for path, dirs, files in os.walk("assets/"):
+            for filename in files:
+                cp_cmd = "cp assets/" + filename + " " + android_asset_path
+                print_run_msg(cp_cmd)
+                os.system(cp_cmd)
 
-    # re-create the .gitkeep file in the assets dir (it is not necessary for the
-    # app but keeps that dir in the git repo)
-    os.system("touch " + android_asset_path + ".gitkeep")
-
-    for path, dirs, files in os.walk("assets/"):
-        for filename in files:
-            cp_cmd = "cp assets/" + filename + " " + android_asset_path
-            print_run_msg(cp_cmd)
-            os.system(cp_cmd)
-
-    print("Step 8b: Update Android app nzsl.dat")
-    android_db_path = options.android + "/app/src/main/assets/db/"
-    if not os.path.exists(android_db_path):
-        os.makedirs(android_db_path)
-
-    os.system("cp nzsl.dat " + android_db_path)
+    if "8b" not in skip:
+        print("Step 8b: Update Android app nzsl.dat")
+        android_db_path = options.android + "/app/src/main/assets/db/"
+        if not os.path.exists(android_db_path):
+            os.makedirs(android_db_path)
+        os.system("cp nzsl.dat " + android_db_path)
 
 if options.cleanup:
     print("Step 9: Cleanup")
